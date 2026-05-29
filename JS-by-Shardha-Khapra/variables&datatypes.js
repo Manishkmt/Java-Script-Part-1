b970870d8aa464c5bc3a541cd151d412261d5eb0
Manish Kumawat <manishkmt211@gmail.com>
2026-05-29 07:08:39 +0530

    Initial commit

---
+++ JS-by-Shardha-Khapra/variables&datatypes.js
@@ -0,0 +1,88 @@
+
+
+// ! What is server side Scripting Language?
+//?  A server side Scripting language is a programming language that runs on the server(backend) to process requests, handle database, authentician,APIs, etc.
+
+//! What is JavaScript:-
+// ?                    JS is a programming language. We use it to give instructions to the computer.
+
+// input(code) -->  Computer  --> Output
+
+// ! Alert in js:-
+//?               alert() is a built-in JavaScript function used to display a popup message box in the browser. 
+
+// alert("Welcome back");
+
+// ! Console.log:-
+// ?              Console.log is used to log(print) a message to the console.
+// Example:- console.log("Manish Kuamwat")
+
+// console.log("Manish Kumawat")
+
+
+// ! Variables:-
+// ?             Variables are containers for data who store the values or objects.
+//Example:- variable name = value
+
+// name = "Manish Kumawat";
+// age = 19;
+// x = null;
+// y = undefined;
+// isFollow = true
+
+// console.log(name);
+// console.log(age);
+// console.log(x);
+// console.log(y);
+// console.log(isFollow);
+
+
+//! Let, Const and var:-
+
+//? 1. var:- 
+//          Variable can br re-declared & updated. A global scope variable.
+
+// var name = "Manish Kumawat";
+// var age = 19;
+// var age = 24;
+// console.log(name);
+// console.log(age);
+
+
+//? 2. let:-
+//          Variable cannot be re-declared but can updated. A block scope variable.
+
+// let name = "Manish Kumawat";
+// let age = 19;
+// console.log(name);
+// console.log(age);
+
+// {
+//   let age = 24;
+//   console.log(age);
+// }
+
+//? 3. const:-
+//             Variable cannot be re-declared or updated. A block scope variable.
+
+// const name = "Manish Kumawat";
+// const age = 19;
+
+// console.log(name);
+// console.log(age);
+
+
+//! Data Types:-
+//             1. numbwer  2. string  3. Boolean  4. Undefined  5. Null  6. BigInt  7. Symbol
+
+//? Types of datatypes:-
+//                    1. primitive:-    1. numbwer  2. string  3. Boolean  4. Undefined  5. Null  6. BigInt  7. Symbol
+
+age = 19;
+name = "Manish Kumawat";
+console.log(age);
+console.log(typeof(age));
+console.log(name);
+console.log(typeof(name));
+
+//                    2. Non Primitive:- objects
