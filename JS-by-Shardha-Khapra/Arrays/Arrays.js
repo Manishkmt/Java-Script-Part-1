04dac0f8f3e94104ec0bed5ba957eb6d0b8472d3
Manish Kumawat <manishkmt211@gmail.com>
2026-06-07 06:57:27 +0530

    Initial commit

--- JS-by-Shardha-Khapra/Arrays/Arrays.js
+++ JS-by-Shardha-Khapra/Arrays/Arrays.js
@@ -31,8 +31,87 @@
 //   console.log(el);
 // }
 
-let cities = ["jaipur", "kuchaman", "sikar", "jhunjhunu"];
+// let cities = ["jaipur", "kuchaman", "sikar", "jhunjhunu"];
 
-for(let city of cities){
-  console.log(city.toUpperCase());
-}
+// for(let city of cities){
+//   console.log(city.toUpperCase());
+// }
+
+
+// ! Array methods:-
+// ? 1. Push():-    add to end.
+
+//  Example:-
+// let foodItems = ["Mango", "Onion", "Apple", "pizza"];
+// console.log(foodItems);
+
+// foodItems.push("Pineapple");
+// console.log(foodItems);
+
+
+// ? 2. Pop():-  delete from end & return.
+
+
+// let foodItems = ["Mango", "Onion", "Apple", "pizza"];
+// console.log(foodItems);
+
+// let deletedItem = foodItems.pop();
+// console.log(foodItems);
+// console.log(deletedItem);
+
+
+
+// ? 3. toString():-  converts array to string
+
+// let foodItems = ["Mango", "Onion", "Apple", "pizza"];
+// let marks = [87, 98, 69, 100, 89];
+// marks.toString();
+
+
+// console.log(foodItems);
+
+// foodItems.toString();
+// console.log(foodItems.toString());
+// console.log(marks.toString());
+
+
+// ?4. Concat():- joins multiple arrays & return result
+
+//  Example:-
+// let marks1 = [98, 97, 78,56, 45, 67];
+// let marks2 = [90, 76, 34, 56, 70];
+// let marks = marks1.concat(marks2);
+// console.log(marks);
+
+
+// ? 5. unshift():- add to start
+
+// let foodItems = ["Mango", "Onion", "Apple", "pizza"];
+// foodItems.unshift("Burger");
+// console.log(foodItems);
+
+// ? 6. shift():- delete from start & return
+
+// let foodItems = ["Mango", "Onion", "Apple", "pizza"];
+// foodItems.shift();
+// console.log(foodItems);
+
+// ? 7. slice():- returns a piece of array. Don't change in origional array.
+
+// slice(startindex, end index)
+// let foodItems = ["Mango", "Onion", "Apple", "pizza"];
+// console.log(foodItems);
+// console.log(foodItems.slice(1,3));
+
+
+
+
+// ? 8. splice():- change origional array(add, remove, replace)
+
+// Splice(startindex, delCount, newel)
+
+let foodItems = ["Mango", "Onion", "Apple", "pizza"];
+console.log(foodItems);
+console.log(foodItems.splice(1,1,"Guava","Icecream"));
+
+console.log(foodItems.splice(1, 0 ,"Icecream"));
--- JS-by-Shardha-Khapra/Arrays/practice2.js
+++ JS-by-Shardha-Khapra/Arrays/practice2.js
@@ -19,3 +19,5 @@ for(let i = 0; i < prices.length; i++){
 
 }
 console.log(prices);
+
+
