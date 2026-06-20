9dfba7034cb4cd8d27a794bbb21f23643406cc3b
Manish Kumawat <manishkmt211@gmail.com>
2026-06-20 07:14:16 +0530

    Initial commit

--- JS-by-Shardha-Khapra/Document Object Model/dom.js
+++ JS-by-Shardha-Khapra/Document Object Model/dom.js
@@ -1,2 +1,53 @@
 // ! Document Object Model:-
-// ?                        
+// ?                        DOM (Document Object Model) is a programming interface that represents an HTML document as a tree of objects. It allows JavaScript to access and modify HTML elements, attributes, and styles.
+
+
+
+// ! Window Object:-
+// ?                 The window object represents an open window in a browser. It is browser's object (not JavaScript's) & is automatically created by browser.
+// ? It is a global object with lots of properties & methods.
+
+
+// ! What is DOM ? :-
+// ?                  When a web page is loaded, the browser creates a Document Object Model of page
+ 
+//  Document
+//     |
+//    HTML
+//    /  \
+// Head   Body
+//  |      |
+// Title   Div
+//           / \
+//         H1   P
+//          |    |
+//        Text  Text
+
+
+
+// console.dir(document.body);
+
+
+
+// ! DOM Manipulation:-
+
+// ? Selecting wit id:-
+
+document.getElementById("myId")
+
+// ? Selecting with class:-
+
+document.getElementsByClassName("myClass")
+
+
+// ? Selecting with tag:-
+
+document.getElementsByTagName("p")
+
+// ? Query Selector:-
+
+document.querySelector("myId/ myClass/ tag")
+//  returns first element
+
+document.querySelectorAll("myId/ myClass/ tag")
+//  returns a Nodelist
---
+++ JS-by-Shardha-Khapra/Document Object Model/script.js
@@ -0,0 +1,26 @@
+// ? Selecting by id
+
+// let heading = document.getElementById("heading");
+
+// console.dir(heading);
+
+// ? Selecting by class
+
+
+// let headings = document.getElementsByClassName("class");
+// console.dir(headings);
+// console.log(headings);
+
+// ? electing by tag
+
+// let heading = document.getElementsByTagName("p");
+
+// console.dir(heading);
+
+
+// ? Query selector
+let firstelement = document.querySelector("p");
+console.dir(firstelement);
+
+let allelements = document.querySelectorAll("p");
+console.dir(allelements);
